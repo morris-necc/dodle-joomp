3ec9b99d3bad2217afa7cd4f9d81ad8cda9ea98e
morris-necc <morrissskim@gmail.com>
2023-05-05 21:47:43 +0900

    Pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame
+
+#pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    #event handler
+    for event in pygame.event.get():
+        #player clicks the X button
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(60) #60 fps limit
+    
+    #Hi lim!
+    #Don't worry I haven't started on anything
+    #I haven't even read the docs
+    #For now I've just copy/pasted the basic setup from the pygame docs
+
+pygame.quit()
