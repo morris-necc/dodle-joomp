851cbec4f6b32a092b18b692bcd572de20ab795e
morris-necc <morrissskim@gmail.com>
2023-05-15 00:04:08 +0900

    Character movement & gravity

---
+++ classes/player.py
@@ -0,0 +1,56 @@
+import pygame
+import os
+import sys
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.direction = 0
+        self.gravityConstant = 12.5
+        self.jumpForce = 10
+        self.yspeed = 0
+        self.grounded = True
+        jump_path = os.path.join(sys.path[0], "assets", "sprites", "Pirate Bomb", "Sprites", "1-Player-Bomb Guy", "4-Jump")
+        self.images = [pygame.image.load(os.path.join(jump_path, f"{i}.png")) for i in range(1, 4)]
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.frame = 0
+
+    def move(self, steps):
+        self.direction = steps
+        self.rect.x += steps
+
+    def jump(self):
+        self.yspeed -= self.jumpForce
+
+    def update(self, dt):
+        #movement due to gravity
+        if self.grounded:
+            self.jump()
+            self.grounded = False
+
+        #Make bottom of screen ground, for testing purposes
+        if self.rect.y > 740 and self.yspeed > 0:
+            self.grounded = True
+            self.rect.y = 740
+            self.yspeed = 0
+        else:
+            self.yspeed += self.gravityConstant * dt
+        
+        #move the player down
+        self.rect.y += self.yspeed
+        if 0 < self.yspeed <= 4:
+            self.frame = 1
+        elif self.yspeed > 4:
+            self.frame = 2
+        elif self.yspeed <= 0:
+            self.frame = 0
+
+        #left
+        if self.direction < 0:
+            self.image = pygame.transform.flip(self.images[self.frame], True, False)
+
+        #right
+        if self.direction > 0:
+            print(self.frame)
+            self.image = self.images[self.frame]
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
-import os
-import sys
+import classes.player as PLAYER
 
 #pygame setup
 pygame.init()
 screen = pygame.display.set_mode((480, 800))
 clock = pygame.time.Clock()
 running = True
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-sprite_dir = os.path.join(sys.path[0], "assets", "sprites", "Pirate Bomb", "Sprites", "1-Player-Bomb Guy", "1-Idle", "1.png")
-player = pygame.image.load(sprite_dir)
+player = PLAYER.Player()
+player.rect.x = 0
+player.rect.y = 700
+player_group = pygame.sprite.Group()
+player_group.add(player)
+steps = 10
 while running:
     #event handler
     for event in pygame.event.get():
@@ -19,18 +21,17 @@ while running:
 
     screen.fill((0, 0, 0))
 
-    clock.tick(60) #60 fps limit
-    screen.blit(player, player_pos)
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_a] and player_pos.x > 0:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d] and player_pos.x < 425:
-        player_pos.x += 300 * dt
+    if keys[pygame.K_a] and player.rect.x > 0:
+        player.move(-steps)
+    if keys[pygame.K_d] and player.rect.x < 420:
+        player.move(steps)
 
-    pygame.display.update()
+    
+    clock.tick(60) #60 fps limit
     dt = clock.tick(60) / 1000
+    player.update(dt)
+    player_group.draw(screen)
+    pygame.display.update()
 
 pygame.quit()
